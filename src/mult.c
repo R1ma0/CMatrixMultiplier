68cdc5228720c36655d501388075223e451f1544
R1ma0 <cactus-2015@mail.ru>
2023-04-01 15:35:57 +0300

    Added the function of multiplication and calculation of execution time

--- src/mult.c
+++ src/mult.c
@@ -1,2 +1,17 @@
 #include "mult.h"
 
+void multMultiplyIntMatrices(int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
+{
+	for (int r = 0; r < aRows; r++)
+	{
+		for (int c = 0; c < bCols; c++)
+		{
+			matC[r][c] = 0;
+			// aCols == bRows
+			for (int i = 0; i < aCols; i++)
+			{
+				matC[r][c] += matA[r][i] * matB[i][c];
+			}
+		}
+	}
+}
--- src/mult.h
+++ src/mult.h
@@ -1,5 +1,6 @@
 #ifndef MULT_H
 #define MULT_H
 
+void multMultiplyIntMatrices(int **, int **, int **, int, int, int, int);
 
 #endif // MULT_H
