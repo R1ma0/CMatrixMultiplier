af0e64eeda4690912570956eb0f8835bbd79943f
namar <skidanroma@yandex.ru>
2023-04-10 12:57:33 +0300

    fixed 3,4 methods and now 3 method is the fastest

--- src/main.c
+++ src/main.c
@@ -51,36 +51,36 @@ int main(int argc, char **argv)
 	//matIntDisplay(matB, matBRows, matBCols);
 
 	// MATRICES MULTIPLICATION
-	//clock_t begin = clock();
-	double time = multMultiplyIntMatrices(matA, matB, matC, matARows, matACols, matBRows, matBCols);
-	//clock_t end = clock();
-	printf("Code work time : %f sec\n", time);
+	clock_t begin = clock();
+	multMultiplyIntMatrices(matA, matB, matC, matARows, matACols, matBRows, matBCols);
+	clock_t end = clock();
+	printf("Code work time : %f sec\n", calcCodeSectionDeltaTime(begin, end));
 	//matIntDisplay(matC, matARows, matBRows);
 
     int *nowcolumn = (int *)calloc(matBRows, sizeof(int));
-    //begin = clock();
-	time = multMultiply2IntMatrices(nowcolumn,matA, matB, matC, matARows, matACols, matBRows, matBCols);
-    //end = clock();
+    begin = clock();
+	multMultiply2IntMatrices(nowcolumn,matA, matB, matC, matARows, matACols, matBRows, matBCols);
+    end = clock();
     free(nowcolumn);
-	printf("Second variant code work time : %f sec\n", time);
+	printf("Second variant code work time : %f sec\n", calcCodeSectionDeltaTime(begin, end));
     //matIntDisplay(matC, matARows, matBRows);
 
 	int *nowrow = (int *)calloc(matACols, sizeof(int));
-	//begin = clock();
-	time = multMultiply3IntMatrices(nowrow,matA, matB, matC, matARows, matACols, matBRows, matBCols);
-    //end = clock();
+	begin = clock();
+	multMultiply3IntMatrices(nowrow,matA, matB, matC, matARows, matACols, matBRows, matBCols);
+    end = clock();
     free(nowrow);
-	printf("Third variant code work time : %f sec\n", time);
+	printf("Third variant code work time : %f sec\n", calcCodeSectionDeltaTime(begin, end));
 	//matIntDisplay(matC, matARows, matBRows);
 
 	nowcolumn = (int *)calloc(matBRows, sizeof(int));
 	nowrow = (int *)calloc(matACols, sizeof(int));
-	//begin = clock();
-	time = multMultiply3IntMatrices(nowrow,matA, matB, matC, matARows, matACols, matBRows, matBCols);
-    //end = clock();
+	begin = clock();
+	multMultiply4IntMatrices(nowcolumn,nowrow,matA, matB, matC, matARows, matACols, matBRows, matBCols);
+    end = clock();
     free(nowrow);
     free(nowcolumn);
-	printf("Fourth variant code work time : %f sec\n", time);
+	printf("Fourth variant code work time : %f sec\n", calcCodeSectionDeltaTime(begin, end));
 	//matIntDisplay(matC, matARows, matBRows);
 
 
--- src/mult.c
+++ src/mult.c
@@ -1,16 +1,7 @@
 #include "mult.h"
 #include <malloc.h>
-#include <stdio.h>
-#include <time.h>
-
-double multMultiplyIntMatrices(int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
+void multMultiplyIntMatrices(int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
 {
-	clock_t time = 0.0;
-	clock_t begin;
-	clock_t end;
-
-	begin = clock();
-	
 	for (int r = 0; r < aRows; r++)
 	{
 		for (int c = 0; c < bCols; c++)
@@ -23,86 +14,64 @@ double multMultiplyIntMatrices(int **matA, int **matB, int **matC, int aRows, in
 			}
 		}
 	}
-
-	end = clock();
-	
-	return (double)(end - begin) / CLOCKS_PER_SEC;
 }
 
-double multMultiply2IntMatrices(int *nowcolumn, int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
+void multMultiply2IntMatrices(int *nowcolumn, int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
 {
-	clock_t time = 0.0;
-	clock_t begin;
-	clock_t end;
 
-	begin = clock();
-	
 	for (int r = 0; r < aRows; r++)
 	{
 		for (int c = 0; c < bCols; c++)
 		{
 			matC[r][c] = 0;
 			// aCols == bRows
-			for (int i = 0; i < bRows; i++) 
+			for (int i = 0; i < bRows; i++) //Проходим по столбцу в матрице-Б
 			{
 				nowcolumn[i] = matB[i][c];
+			}
+			for (int i = 0; i < aCols; i++)
+			{
 				matC[r][c] += matA[r][i] * nowcolumn[i];
 			}
 		}
 	}
-
-	end = clock();
-
-	return (double)(end - begin) / CLOCKS_PER_SEC;
 }
 
-double multMultiply3IntMatrices(int *nowrow, int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
+void multMultiply3IntMatrices(int *nowrow, int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
 {
-	clock_t time = 0.0;
-	clock_t begin;
-	clock_t end;
-
-	begin = clock();
-	
-
 	for (int r = 0; r < aRows; r++)
 	{
+	    for (int i = 0; i < aCols; i++) //Проходим по строке в матрице-А
+			{
+				nowrow[i] = matA[r][i];
+			}
 		for (int c = 0; c < bCols; c++)
 		{
 			matC[r][c] = 0;
 			// aCols == bRows
-			for (int i = 0; i < aCols; i++) 
-			{
-				nowrow[i] = matA[r][i];
-			}
+
 			for (int i = 0; i < aCols; i++)
 			{
 				matC[r][c] += nowrow[i] * matB[i][c];
 			}
 		}
 	}
-
-	end = clock();
-	return (double)(end - begin) / CLOCKS_PER_SEC;
 }
 
-double multMultiply4IntMatrices(int *nowcolumn, int *nowrow, int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
+void multMultiply4IntMatrices(int *nowcolumn, int *nowrow, int **matA, int **matB, int **matC, int aRows, int aCols, int bRows, int bCols)
 {
-	clock_t time = 0.0;
-	clock_t begin;
-	clock_t end;
-
-	begin = clock();
-
 	for (int r = 0; r < aRows; r++)
 	{
+	    for (int i = 0; i < aCols; i++) //Проходим по строке в матрице-А
+			{
+				nowrow[i] = matA[r][i];
+			}
 		for (int c = 0; c < bCols; c++)
 		{
 			matC[r][c] = 0;
 			// aCols == bRows
-			for (int i = 0; i < aCols; i++) 
+			for (int i = 0; i < aCols; i++) //Проходим по строке в матрице-А
 			{
-				nowrow[i] = matA[r][i];
 				nowcolumn[i] = matB[i][c];
 
 			}
@@ -112,7 +81,4 @@ double multMultiply4IntMatrices(int *nowcolumn, int *nowrow, int **matA, int **m
 			}
 		}
 	}
-	
-	end = clock();
-	return (double)(end - begin) / CLOCKS_PER_SEC;
 }
